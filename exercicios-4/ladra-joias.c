c997ccd9649ce4e3ee052c25416d216e4ce98cd8
Victor Queiroz <victoorqrz@gmail.com>
2023-05-10 01:04:09 -0300

    ladra-joias corrigido

--- exercicios-4/ladra-joias.c
+++ exercicios-4/ladra-joias.c
@@ -1,56 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
-#include <limits.h>
 
-int n, counter=0, aux=0, joias[1000001];
+int max(int a, int b) {
+    return a > b ? a : b;
+}
 
-int max_3(int n1, int n2, int n3){
-  if(n1 > n2){
-    if(n1 > n3) return n1;
-    else {
-      if(n2 > n3) return n2;
-      else return n3;
+int main() {
+    int n;
+    scanf("%d", &n);
+    int *joias = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &joias[i]);
     }
-  } else {
-    if(n2 > n3) return n2;
-    else return n3;
-  } 
-}
-
-int dp(int i){
-  if (i == 0){
-    aux+=joias[i];
-    return aux;
-  } else {
-      aux+=joias[i];
-      if(i - 3 >= 0){
-        dp(i-3);
-      } else return aux;
-  }
-}
-
-int main(void){
-
-  scanf("%d", &n);
-
-  for (int i = 0; i < n; i++)
-  {
-    scanf("%d", &joias[i]);
-  }
-
-  if(n <= 3){
-    if(n == 1){
-      printf("%d", joias[0]);
-    } else if(n == 2){
-      printf("%d", max_3(joias[0], joias[1], 0));
-    } else {
-      printf("%d", max_3(joias[0], joias[1], joias[2]));
-    } 
-  } else {
-    printf("%d %d %d",dp(n-3), dp(n-2), dp(n-1));
-  }
-
-  return 0;
+    int *dp = malloc(n * sizeof(int));
+    dp[0] = joias[0];
+    dp[1] = max(joias[0], joias[1]);
+    dp[2] = max(max(joias[0], joias[1]), joias[2]);
+    for (int i = 3; i < n; i++) {
+        dp[i] = max(max(joias[i] + dp[i - 3], dp[i - 2]), dp[i - 1]);
+    }
+    printf("%d\n", dp[n - 1]);
+    free(joias);
+    free(dp);
+    return 0;
 }
